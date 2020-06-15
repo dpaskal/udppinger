9a46be39f2142aea8c7746bd072459e9fb4b4073
Daniel Paskalev <dpaskal1@binghamton.edu>
2020-06-15 10:09:40 -0400

    add files

---
+++ client.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <stdlib.h> 
+#include <unistd.h> 
+#include <string.h> 
+#include <time.h> 
+#include <sys/types.h> 
+#include <sys/socket.h> 
+#include <arpa/inet.h> 
+#include <netinet/in.h> 
+
+#define PORT	 12000
+
+int main() {
+    
+
+    return 0;
+}
