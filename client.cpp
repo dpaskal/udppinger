61faa90288c114a4ac3f65024523f3e33f6c4e51
Daniel Paskalev <dpaskal1@binghamton.edu>
2020-06-16 12:31:06 -0400

    loop added

--- client.cpp
+++ client.cpp
@@ -12,7 +12,7 @@
 
 
 #define PORT		12000
-#define NUM_MSGS	10
+#define MAX_MSGS	10
 
 int main() {
 	int sockfd, n;
@@ -30,25 +30,27 @@ int main() {
 	servaddr.sin_addr.s_addr = INADDR_ANY; // localhost
 	servaddr.sin_port = htons(PORT); // port number
 
-	auto start = std::chrono::high_resolution_clock::now(); // timer start
-	// Send a packet to server
-	n = sendto(sockfd, (char *) buffer, sizeof(buffer),
-		MSG_CONFIRM, (struct sockaddr *) &servaddr, sizeof(servaddr));
-	if (n<0){
-		std::cout << "sendto failed: " << strerror(errno) << std::endl;
-		exit(0);
+	for(int i=0; i<MAX_MSGS; i++) {
+		auto start = std::chrono::high_resolution_clock::now(); // timer start
+		// Send a packet to server
+		n = sendto(sockfd, (char *) buffer, sizeof(buffer),
+			MSG_CONFIRM, (struct sockaddr *) &servaddr, sizeof(servaddr));
+		if (n<0){
+			std::cout << "sendto failed: " << strerror(errno) << std::endl;
+			exit(0);
+		}
+		// std::cout << n << " bytes sent." << std::endl;
+
+		n = recvfrom(sockfd, (char *)buffer, sizeof(buffer),
+			MSG_WAITALL, (struct sockaddr *) &servaddr, &len);
+		buffer[n] = '\0';
+		// std::cout << "Client received: " << buffer << std::endl;
+		auto end = std::chrono::high_resolution_clock::now(); // timer end
+		double duration = std::chrono::duration_cast<std::chrono::nanoseconds>(end-start).count();
+		duration *= 1e-9; // move nano seconds to seconds
+		std::cout << "RTT_" << i+1 << " = " << duration << std::setprecision(9) << "s" << std::endl;
+
 	}
-	std::cout << n << " bytes sent." << std::endl;
-
-	n = recvfrom(sockfd, (char *)buffer, sizeof(buffer),
-		MSG_WAITALL, (struct sockaddr *) &servaddr, &len);
-	buffer[n] = '\0';
-	std::cout << "Client received: " << buffer << std::endl;
-	auto end = std::chrono::high_resolution_clock::now(); // timer end
-	double duration = std::chrono::duration_cast<std::chrono::nanoseconds>(end-start).count();
-	duration *= 1e-9; // move nano seconds to seconds
-	std::cout << "RTT = " << duration << std::setprecision(9) << "s" << std::endl;
-
 	close(sockfd);
 	return 0;
 }
